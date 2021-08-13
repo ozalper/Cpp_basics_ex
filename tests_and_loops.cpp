3318683f74c174403e45bb0b54bcaec69055a6fa
Alper OZ <alperoz058@gmail.com>
2021-08-13 13:45:15 +0300

    the while function option added

--- tests_and_loops.cpp
+++ tests_and_loops.cpp
@@ -5,7 +5,7 @@ using namespace std;
 bool accept(){                                      // Writing with if-else statements
     cout << "Do you want to proceed(y or n)?\n";
     char answer = 0;
-    cin >> answer;
+    cin >> answer;                                  // read the answer
 
     if(answer == 'y'){
         return true;
@@ -17,7 +17,7 @@ bool accept(){                                      // Writing with if-else stat
 bool accept2(){                                     // Writing with switch-case statemens
     cout << "Dou you want to proceed(y or n)?\n";
     char answer = 0;
-    cin >> answer;
+    cin >> answer;                                  // read the answer
 
     switch(answer){
         case 'y':
@@ -33,6 +33,30 @@ bool accept2(){                                     // Writing with switch-case 
     }
 }
 
+bool accept3(){                                     //Writing with while statements
+    cout << "Do you want to proceed(y or n)?\n";
+    char answer = 0;
+    cin >> answer;                                  // read the answer
+    int tries = 0;
+    while(tries<4){
+        switch (answer)
+        {
+        case 'y':
+            return true;
+            break;
+        case 'n':
+            return false;
+            break;
+        default:
+            cout << "Sorry, I don't understand please try again...\n";
+            tries++;                                // increment
+            break;
+        }
+    }
+    cout << "I'll take that no..\n";
+    return false;
+}
+
 int main(){
-    accept();
+    
 }
